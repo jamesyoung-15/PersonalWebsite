dadd3fefd0f37f6c2aab10a40b3d1f256191ece5
James Young <jamesyoung3931@gmail.com>
2023-12-24 03:09:53 +0800

    sections activate

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,46 @@
+
+
+/* Navbar section */
+
 const navMenu = document.getElementById("nav-menu");
 const navToggle = document.getElementById("nav-toggle");
 const navClose = document.getElementById("nav-close");
-
+// show menu
 if(navToggle){
     navToggle.addEventListener('click', () => {
         navMenu.classList.add("show-menu");
     })
 }
-
+// close menu
 if(navClose){
     navClose.addEventListener('click', () => {
         navMenu.classList.remove("show-menu");
     })
 }
+
+/* Show active scroll section */
+
+window.addEventListener("scroll", ()=>{
+    const scrollPosition = window.scrollY;
+    const sections = document.querySelectorAll("main section[id]");
+    const navLinks = document.querySelectorAll(".nav-list a");
+
+    sections.forEach((section)=>{
+        const sectionTop = section.offsetTop-600;
+        const sectionHeight = section.offsetHeight;
+        const sectionId = section.getAttribute('id');
+         // Check if the current section is in the viewport
+        if (scrollPosition >= sectionTop && scrollPosition < sectionTop + sectionHeight) {
+            // Add the 'active' class to the corresponding navbar link
+            navLinks.forEach((link)=>{
+                // console.log(sectionId);
+                if(link.getAttribute('href')=== `#${sectionId}`){
+                    link.classList.add('active-link');
+                }
+                else{
+                    link.classList.remove('active-link');
+                }
+            })
+        }
+    })  
+});
